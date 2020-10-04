b14fc2d1e782220e84853da1398d2fe7d40eecfa
Mateusz <mateusz@Mateusz-2.local>
2020-10-04 22:04:48 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+document.getElementById("defaultOpen").click();
+
+
+function openCity(evt, cityName) {
+    var i, fleetTabContent, fleetTabLinks;
+    fleetTabContent = document.getElementsByClassName("fleet-tab-content");
+    for (i = 0; i < fleetTabContent.length; i++) {
+      fleetTabContent[i].style.display = "none";
+    }
+  
+    fleetTabLinks = document.getElementsByClassName("fleet-tab-links");
+    for (i = 0; i < fleetTabLinks.length; i++) {
+      fleetTabLinks[i].className = fleetTabLinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
